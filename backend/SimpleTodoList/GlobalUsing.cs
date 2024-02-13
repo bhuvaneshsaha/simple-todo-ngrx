976370ea516e4d9c85603257a17750aa2fb0d1d9
Bhuvanesh <bhuvaneshmib@gmail.com>
2024-02-13 13:19:49 +0530

    Remove old api project

--- backend/SimpleTodoList/GlobalUsing.cs
+++
@@ -1,8 +0,0 @@
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
-
-global using GuardNet;
-
-global using SimpleTodoList.Models.Entities;
-global using SimpleTodoList.Data;
-global using SimpleTodoList.Repos;
