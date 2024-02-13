254aa315f9ecb649db8cc7cf00794f3eb38b226b
Bhuvanesh <bhuvaneshmib@gmail.com>
2024-02-13 13:08:59 +0530

    Add Migrations

---
+++ backend/SimpleTodoList.API/Helpers/Extensions/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTodoList.App.Services;
+using SimpleTodoList.Core.Interfaces;
+using SimpleTodoList.Infra.Data;
+using SimpleTodoList.Infra.Repos;
+
+namespace SimpleTodoList.API.Helpers.Extensions;
+
+public static class DependencyInjection
+{
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TodoDbContext>(options =>
+        {
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", config =>
+            {
+                config.AllowAnyOrigin();
+                config.AllowAnyMethod();
+                config.AllowAnyHeader();
+            });
+        });
+
+        services.AddScoped<ITodoRepo, TodoRepo>();
+        services.AddScoped<ITodoService, TodoService>();
+    }
+}
